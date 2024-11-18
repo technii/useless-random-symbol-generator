7bd930159555737b43e9adc4fb84177259de3d51
techni <145453745+technii@users.noreply.github.com>
2024-11-18 19:07:11 +0000

    added the one thing like its one thing thats it

---
+++ funny goofy random symbol generator.py
@@ -0,0 +1,13 @@
+import random
+
+amount = 0
+letters = ["!","£","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]",":",";","@","'","<",",",".","?","/"]
+output = []
+amount = input("imput amount of characters you wish for -> ") 
+foram = int(amount)
+while foram > 0:
+    arraynumber = random.randrange(0,25)
+    output.append(letters[int(arraynumber)])
+    foram = foram - 1
+printoutput = "".join(output)
+print("output =",printoutput)
